4cfcfddc12c55c90f17030699896a4235228a072
Rosterrr <rodrigochanta331@gmail.com>
2025-01-26 16:51:11 -0600

    Delete wdd231 directory

--- wdd231/scripts/getdate.js
+++
@@ -1,5 +0,0 @@
-// Get the current year and update the footer's first paragraph
-document.getElementById('currentyear').textContent = new Date().getFullYear();
-
-// Get the document's last modified date and update the second paragraph
-document.getElementById('lastModified').textContent = `Last modified: ${document.lastModified}`;
